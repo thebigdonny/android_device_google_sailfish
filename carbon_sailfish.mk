b76d34b37b9ee51f4ae6b173fb146787a9ffa945
Christian Oder <myself5@carbonrom.org>
2016-12-23 21:28:24 +0100

    sailfish: Carbonify

--- carbon_sailfish.mk
+++ carbon_sailfish.mk
@@ -17,12 +17,20 @@
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
+# Screen Resolution for the Bootanimation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
 # Inherit aosp_sailfish
 $(call inherit-product, device/google/marlin/aosp_sailfish.mk)
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
-PRODUCT_NAME := du_sailfish
+# Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
+# Inherit Carbon product configuration
+$(call inherit-product, vendor/carbon/config/common.mk)
+
+PRODUCT_NAME := carbon_sailfish
 PRODUCT_DEVICE := sailfish
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel
@@ -30,6 +38,5 @@ PRODUCT_MANUFACTURER := Google
 
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=sailfish \
     BUILD_FINGERPRINT=google/sailfish/sailfish:7.1.1/NMF26Q/3537182:user/release-keys \
     PRIVATE_BUILD_DESC="sailfish-user 7.1.1 NMF26Q 3537182 release-keys"
